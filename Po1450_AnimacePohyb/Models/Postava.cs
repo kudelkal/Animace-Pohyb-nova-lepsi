14287c96f3188c81e4b87e66a24175910df57f22
Lukáš Kudělka <kudelkal@office365.zcu.cz>
2024-04-22 16:23:53 +0200

    Přidejte soubory projektu.

---
+++ Po1450_AnimacePohyb/Models/Postava.cs
@@ -0,0 +1,81 @@
+﻿namespace Po1450_AnimacePohyb.Models
+{
+    public class Postava
+    {
+        public Postava(string nazev, string cestaObrazek, int width) 
+        { 
+            Nazev = nazev;
+            CestaObrazek = cestaObrazek;
+            AktualniPozice = -1;
+            Width = width;
+        }
+        public string Nazev { get; private set; }
+        public string CestaObrazek { get; private set; }
+        private List<Souradnice> Pozice { get; set; } = new List<Souradnice>();
+        public int AktualniPozice { get; private set; }
+        public int Width { get; set; }
+        private bool JduDopredu { get; set; } = true;
+
+        private bool HlavaVpravo { get; set; } = true;
+
+        public string Style
+        {
+            get
+            {
+                if (AktualniPozice < 0)
+                {
+                    return $" width: {Width}px;";
+                }
+                else
+                {
+                    var poz = Pozice[AktualniPozice];
+                    return poz.Style + $" width: {Width * poz.VelikostObrazku / 100}px; ";
+                }
+                
+                
+            }
+        }
+
+        public string TransformRotateY => HlavaVpravo ? "transform: rotateY(0deg);" : "transform: rotateY(180deg);";
+        
+
+
+        public void PridejPozici(int pozX, int pozY, int velikostObrazku)
+        {
+            var souradnice = new Souradnice(pozX, pozY, velikostObrazku);
+            Pozice.Add(souradnice);
+        }
+
+        public void Presun()
+        {
+            if (JduDopredu)
+            {
+                if (AktualniPozice + 1 == Pozice.Count)
+                {
+                    JduDopredu = false;
+                }
+
+            }
+            else
+            {
+                if (AktualniPozice == 0)
+                {
+                    JduDopredu = true;
+                }
+
+            }
+            var predchoziPozice = AktualniPozice;
+
+            if (JduDopredu) 
+            {
+                AktualniPozice++;
+            }
+            else
+            {
+                AktualniPozice--;
+            }
+
+            HlavaVpravo = predchoziPozice<0 || Pozice[predchoziPozice].PozX < Pozice[AktualniPozice].PozX;
+        }
+    }
+}
---
+++ Po1450_AnimacePohyb/Models/Souradnice.cs
@@ -0,0 +1,16 @@
+﻿namespace Po1450_AnimacePohyb.Models
+{
+    public class Souradnice
+    {
+        public Souradnice(int pozX, int pozY, int velikostObrazku) 
+        { 
+            PozX = pozX;
+            PozY = pozY;
+            VelikostObrazku = velikostObrazku;
+        }
+        public int PozX { get; private set; }
+        private int PozY { get; set;}
+        public int VelikostObrazku { get; set; }
+        public string Style => $"left: {PozX}px; top: {PozY}px;";
+    }
+}
